160a9dc1194c0ab9daf4a4a89450af2053f8f9e4
ivan <ivanaguilando6@gmail.com>
2019-01-08 10:54:14 -0600

    LoginPane

---
+++ copesa/copesa/copesa/Infraestructura/InstanceLocator.cs
@@ -0,0 +1,23 @@
+﻿
+namespace copesa.Infraestructura
+{
+    using ViewModels;
+  
+    class InstanceLocator
+    {
+        #region Properties
+        public MainVewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainVewModel();
+        }
+        #endregion
+    }
+}
---
+++ copesa/copesa/copesa/Views/LoginPane.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace copesa.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPane : ContentPage
+	{
+		public LoginPane ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
